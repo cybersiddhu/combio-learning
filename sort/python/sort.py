a77b200a4d1f7072a111eaad5f3fc818eaffd848
Siddhartha Basu <biosidd@gmail.com>
2013-12-23 22:15:26 -0500

    added insertion sort and unit tests

--- sort/python/sort.py
+++ sort/python/sort.py
@@ -6,12 +6,14 @@ def bubblesort_descending(num):
             if num[i] < num[i + 1]:
                 num[i + 1], num[i] = num[i], num[i + 1]
 
+
 def bubblesort_ascending(num):
     """ In place ascending bubble sort """
     for passnum in range(len(num) - 1, 0, -1):
         for i in range(passnum):
             if num[i] > num[i + 1]:
                 num[i + 1], num[i] = num[i], num[i + 1]
+
 
 def selectionsort_ascending(num):
     """
@@ -26,5 +28,31 @@ def selectionsort_ascending(num):
             #find the index with highest value
             if num[loc] > num[maxpos]:
                 maxpos = loc
-            #put them in correct position
+            #put them in correct position by swapping
             num[fillslot], num[maxpos] = num[maxpos], num[fillslot]
+
+
+def insertionsort_ascending(num):
+    """In place insertion sort"""
+    for loc in range(1, len(num)):
+        # this variable(pos) tracks the index as the item in the list
+        # moves back after each comparison
+        pos = loc
+        # this variable(item) holds the item in the list until
+        # it gets the location to be inserted
+        item = num[loc]
+        inner = True
+        while pos > 0 and inner:
+            # this if block keeps shifting item forward
+            if item < num[pos - 1]:
+                #exchange position of items
+                num[pos] = num[pos - 1]
+                pos = pos - 1
+            else:
+                # no more comparison needed, the rest
+                # of the list is already sorted
+                inner = False
+        if loc != pos:
+            num[pos] = item
+
+    return True
---
+++ sort/python/sort_test.py
@@ -0,0 +1,21 @@
+import unittest
+from sort import insertionsort_ascending
+
+
+class SortingTestCase(unittest.TestCase):
+    def test_insertionsort_ascending(self):
+        """test insertionsort algorithm"""
+        alist = [9, 2, 15, 4, 3, 1, 7]
+        newlist = alist[:5]
+        self.assertTrue(insertionsort_ascending(alist),
+                        'should run insertionsort')
+        self.assertListEqual(alist, [1, 2, 3, 4, 7, 9, 15],
+                             'should sorted uneven list')
+        self.assertTrue(insertionsort_ascending(newlist),
+                        'should run insertionsort')
+        self.assertListEqual(newlist, [2, 3, 4, 9, 15],
+                             'should sorted even list')
+
+
+if __name__ == '__main__':
+    unittest.main()
