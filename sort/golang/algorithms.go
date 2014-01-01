f2e22ebedd060dc3f05acf0f78f7eb5fb0d3522a
Siddhartha Basu <biosidd@gmail.com>
2014-01-01 18:00:18 -0500

    Added radixsort algorithm

--- sort/golang/algorithms.go
+++ sort/golang/algorithms.go
@@ -38,3 +38,49 @@ func Merge(left, right []int) []int {
     }
     return result
 }
+
+//RadixSort implements radixsort algorithm, in place sorting of
+//of integer slice
+func RadixSort(nums []int) {
+    base := 10
+
+    //Identify number with most digits
+    max := 0
+    for _, item := range nums {
+        if item > max {
+            max = item
+        }
+    }
+
+    //The loop runs until it reaches maximum position of digit.
+    //It is done by dividing the maximum digit number with the decimal
+    //numeric power. The power increments by a factor of numeric
+    //base(10) on each iteration(1,10,100,1000 etc..)
+    for power := 1; max/power > 0; power *= base {
+        //Create bucket(a 2d slice) list with 10 bucket, each for one digit
+        //in base 10 numeric system
+        var bucket [10][]int
+
+        for _, item := range nums {
+            //Get the digit value of every number starting
+            //from the right(least significant digit)
+            digit := int(item / power % base)
+
+            //Place it on a bucket matching its digit value
+            bucket[digit] = append(bucket[digit], item)
+        }
+        //After each pass of sorting based on digit value
+        //take them of the bucket and put them back in the original
+        //slice
+        index := 0
+        for i := 0; i < len(bucket); i++ {
+            for _, item := range bucket[i] {
+                nums[index] = item
+                index++
+            }
+
+        }
+
+    }
+
+}
--- sort/golang/algorithms_test.go
+++ sort/golang/algorithms_test.go
@@ -11,6 +11,14 @@ func TestMergeSort(t *testing.T) {
     }
 }
 
+func TestRaRadixSort(t *testing.T) {
+    nums := []int{213, 55, 21, 5, 2334, 31, 430, 20}
+    RadixSort(nums)
+    if !Equal(nums, []int{5, 20, 21, 31, 55, 213, 430, 2334}) {
+        t.Error("Could not sort slice using radixsort")
+    }
+}
+
 func Equal(left []int, right []int) bool {
     if len(left) != len(right) {
         return false
